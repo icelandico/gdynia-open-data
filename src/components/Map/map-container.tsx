e2b58c8c7700fc030e1f9b51eef6a1a9101ea15c
Michal Muszynski <m.muszy@gmail.com>
2019-04-27 22:55:57 +0200

    Coordinates definition

--- src/components/Map/map-container.tsx
+++ src/components/Map/map-container.tsx
@@ -5,41 +5,34 @@ import classNames from "classnames"
 import { Map, TileLayer, Marker, Popup } from "react-leaflet"
 import values from "../../global/values"
 
-interface IMapProps {
-}
+interface IMapProps {}
 
 interface MapState {
-  lat: number
-  long: number
   zoom: number
 }
 
 class MapContainer extends React.Component<IMapProps, MapState> {
-
   state = {
-    lat: 54.5,
-    long: 18.5,
     zoom: 12
   }
 
   render() {
-    const position: [number, number] = [this.state.lat, this.state.long]
+    const position = values.centerCoordinates
     return (
       <div>
         <Map
           center={position}
           zoom={this.state.zoom}
           className={MapStyles.main}
-          style={{height: "80vh"}} // Workaround for correct map display
+          style={{ height: "80vh" }} // Workaround for correct map display
         >
-        <TileLayer
-          attribution={values.attribution}
-          url={values.tileSource}
-          ext={values.ext}
-        />
+          <TileLayer
+            attribution={values.attribution}
+            url={values.tileSource}
+            ext={values.ext}
+          />
         </Map>
       </div>
-
     )
   }
 }
--- src/global/values.ts
+++ src/global/values.ts
@@ -1,13 +1,16 @@
 interface IValues {
-  tileSource: string,
-  ext?: string,
+  tileSource: string
+  ext?: string
   attribution: string
+  centerCoordinates: [number, number]
 }
 
 const values: IValues = {
-  tileSource: "https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}",
+  tileSource:
+    "https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}",
   ext: "png",
-  attribution: `&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors`
+  attribution: `&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors`,
+  centerCoordinates: [54.5, 18.5]
 }
 
 export default values
