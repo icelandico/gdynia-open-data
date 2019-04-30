72943f1931f9a3920e3d304f7b468087b4ff58f4
Michal Muszynski <m.muszy@gmail.com>
2019-04-30 22:40:29 +0200

    API fix

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,7 @@
 const proxyUrl = "https://cors-anywhere.herokuapp.com/"
+
+// Weather stations location
+
 const weatheStationsRequest =
   "http://api.zdiz.gdynia.pl/ri/rest/weather_stations"
 
@@ -9,3 +12,18 @@ const getWeatherStations = async (request: RequestInfo): Promise<any> => {
 }
 
 export const weatherStations = getWeatherStations(weatheStationsRequest)
+
+// Weather stations detailed data
+
+const weatheStationsDataRequest =
+  "http://api.zdiz.gdynia.pl/ri/rest/weather_stations_data"
+
+const getWeatherStationsData = async (request: RequestInfo): Promise<any> => {
+  const res = await fetch(`${proxyUrl}${request}`)
+  const json = await res.json()
+  return json
+}
+
+export const weatherStationsData = getWeatherStationsData(
+  weatheStationsDataRequest
+)
