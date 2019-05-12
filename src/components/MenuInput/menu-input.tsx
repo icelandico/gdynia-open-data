4d8edb21a723281c92321b17248233698f9ba6d0
Michal Muszynski <m.muszy@gmail.com>
2019-05-12 20:07:57 +0200

    Working on layer display

--- src/components/MenuInput/menu-input.tsx
+++ src/components/MenuInput/menu-input.tsx
@@ -3,16 +3,42 @@ import styles from "./menu-input-styles"
 import "rbx/index.css"
 import classNames from "classnames"
 
-class MenuInput extends React.Component {
+interface Istate {
+	chosen: string | null
+}
+
+class MenuInput extends React.Component<any, Istate> {
+	state = {
+		chosen: null
+	}
+
+	handleChange = (val: string) => {
+		this.setState({
+			chosen: val
+		})
+	}
+
 	render() {
 		return (
 			<React.Fragment>
 				<label>
-					<input type="radio" className={styles.menuInput} />
+					<input
+						name="chosen-layer"
+						type="radio"
+						className={styles.menuInput}
+						value="none"
+						onChange={() => this.handleChange("none")}
+					/>
 					Brak warstw
 				</label>
 				<label>
-					<input type="radio" className={styles.menuInput} checked />
+					<input
+						name="chosen-layer"
+						type="radio"
+						className={styles.menuInput}
+						onChange={() => this.handleChange("weather")}
+						checked
+					/>
 					Pogoda
 				</label>
 			</React.Fragment>
