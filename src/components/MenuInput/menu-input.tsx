0464bc3a2bd59e7031c96f7f64edce3086648121
Michal Muszynski <m.muszy@gmail.com>
2019-06-07 23:02:48 +0200

    Laco uninstall

--- src/components/MenuInput/menu-input.tsx
+++ src/components/MenuInput/menu-input.tsx
@@ -2,64 +2,64 @@ import * as React from "react"
 import styles from "./menu-input-styles"
 import "rbx/index.css"
 import classNames from "classnames"
-import { useStore } from "laco-react"
-import { LayerStore, CounterStore, increment } from "../../store/app-store"
+import { LayerStore } from "../../store/app-store"
+import { Layer } from "leaflet"
 
 interface Istate {
-	chosen: string | null
+  chosen: string | null
 }
 
 class MenuInput extends React.Component<any, Istate> {
-	state = {
-		chosen: null
-	}
+  state = {
+    chosen: null
+  }
 
-	handleChange = (val: any) => {
-		const value = val.target.value
-		console.log(value)
-		this.setState({
-			chosen: value
-		})
-	}
+  handleChange = (val: any) => {
+    const value = val.target.value
+    console.log(value)
+    this.setState({
+      chosen: value
+    })
+  }
 
-	render() {
-		// const counterState = useStore(CounterStore)
-		return (
-			<React.Fragment>
-				<label>
-					<input
-						name="chosen-layer"
-						type="radio"
-						className={styles.menuInput}
-						value="none"
-						onChange={this.handleChange}
-					/>
-					Brak warstw
+  render() {
+
+    return (
+      <React.Fragment>
+        <label>
+          <input
+            name="chosen-layer"
+            type="radio"
+            className={styles.menuInput}
+            value="none"
+            onChange={this.handleChange}
+          />
+          Brak warstw
 				</label>
-				<label>
-					<input
-						name="chosen-layer"
-						type="radio"
-						className={styles.menuInput}
-						value="weather"
-						onChange={this.handleChange}
-						defaultChecked
-					/>
-					Pogoda
+        <label>
+          <input
+            name="chosen-layer"
+            type="radio"
+            className={styles.menuInput}
+            value="weather"
+            onChange={this.handleChange}
+            defaultChecked
+          />
+          Pogoda
 				</label>
-				<label>
-					<input
-						name="chosen-layer"
-						type="radio"
-						className={styles.menuInput}
-						value="traffic"
-						onChange={this.handleChange}
-					/>
-					Dane o ruchu
+        <label>
+          <input
+            name="chosen-layer"
+            type="radio"
+            className={styles.menuInput}
+            value="traffic"
+            onChange={this.handleChange}
+          />
+          Dane o ruchu
 				</label>
-			</React.Fragment>
-		)
-	}
+      </React.Fragment>
+    )
+  }
 }
 
 export default MenuInput
