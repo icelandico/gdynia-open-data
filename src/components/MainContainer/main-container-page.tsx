e46d2c144ca8fae41e570dcdc99b1f6b806ff743
Michal Muszynski <m.muszy@gmail.com>
2019-06-23 21:31:23 +0200

    Convert to functional components

--- src/components/MainContainer/main-container-page.tsx
+++ src/components/MainContainer/main-container-page.tsx
@@ -7,23 +7,17 @@ import values from "../../global/values"
 import MapContainer from "./../Map/map-container"
 import MenuPanel from "../MenuPanel/menu-panel"
 
-interface IMapProps {}
-
-interface State {}
-
-class MainContainer extends React.Component<IMapProps, State> {
-  render() {
-    return (
-      <Column.Group>
-        <Column size={3}>
-          <MenuPanel />
-        </Column>
-        <Column>
-          <MapContainer />
-        </Column>
-      </Column.Group>
-    )
-  }
+const MainContainer: React.FC = () => {
+  return (
+    <Column.Group>
+      <Column size={3}>
+        <MenuPanel />
+      </Column>
+      <Column>
+        <MapContainer />
+      </Column>
+    </Column.Group>
+  )
 }
 
 export default MainContainer
--- src/components/Map/map-container.tsx
+++ src/components/Map/map-container.tsx
@@ -1,7 +1,10 @@
 import * as React from "react"
+import { useState, useEffect } from "react"
 import MapStyles from "./../Map/map-container-styles"
 import "rbx/index.css"
 import classNames from "classnames"
+import { useStore } from "effector-react"
+import { activeLayer } from "../../store/app-store"
 import { Map, TileLayer, Marker, Popup } from "react-leaflet"
 import L from "leaflet"
 import values from "../../global/values"
@@ -11,26 +14,30 @@ import WeatherPopup from "../Layers/Weather/WeatherPopup/weather-popup"
 
 interface IMapProps {
   station?: IStation
+  mapZoom?: number
+  stations?: IStation[]
 }
 
-interface MapState {
-  zoom: number
-  stations: IStation[]
-}
+const MapContainer: React.FC<IMapProps> = () => {
 
-class MapContainer extends React.Component<IMapProps, MapState, IStation> {
-  state = {
-    zoom: 12,
-    stations: []
-  }
+  const layer = useStore(activeLayer)
+  const [mapZoom, getZoom] = useState(12)
+  const [stations, getStations] = useState([])
 
-  getData = async () => {
+  useEffect(() => {
+    const data = getData()
+    data.then(res => {
+      getStations(res)
+    })
+  }, [])
+
+  const getData = async () => {
     const stations = await weatherStations
     const stationsData = await weatherStationsData
-    return this.concatData(stations, stationsData)
+    return concatData(stations, stationsData)
   }
 
-  concatData = (stations: any, stationsData: []) => {
+  const concatData = (stations: any, stationsData: []) => {
     const wStations = stations.weatherStations
     const wData = wStations.map((item: any) => {
       const matched = stationsData.filter((s: any) => s.weatherStationId === item.id)
@@ -39,51 +46,52 @@ class MapContainer extends React.Component<IMapProps, MapState, IStation> {
     return wData
   }
 
-  componentDidMount() {
-    const data = this.getData()
-    data.then(res => {
-      this.setState({
-        stations: res
-      })
-    })
-  }
+  // componentDidMount() {
+  //   const data = getData()
+  //   data.then(res => {
+  //     this.setState({
+  //       stations: res
+  //     })
+  //   })
+  // }
 
-  convertCoords = (coords: [number, number]) => {
+  const convertCoords = (coords: [number, number]) => {
     return new L.LatLng(coords[1], coords[0])
   }
 
-  renderWeatherStations = () => {
-    const stations: IStation[] = this.state.stations
-    return (stations.map((s: IStation) => {
+  const renderWeatherStations = () => {
+    const stationList: IStation[] = stations
+    return (stationList.map((s: IStation) => {
       const location = s.location.coordinates
       return (
-        <Marker position={this.convertCoords(location)} key={s.id} id={3}>
+        <Marker position={convertCoords(location)} key={s.id} id={3}>
           <WeatherPopup id={s.weatherStationId} street={s.street} airTemperature={s.airTemperature} />
         </Marker>
       )
     }))
   }
 
-  render() {
-    const position = values.centerCoordinates
-    return (
-      <div>
-        <Map
-          center={position}
-          zoom={this.state.zoom}
-          className={MapStyles.main}
-          style={{ height: "80vh" }} // Workaround for correct map display
-        >
-          <TileLayer
-            attribution={values.attribution}
-            url={values.tileSource}
-            ext={values.ext}
-          />
-          {this.renderWeatherStations()}
-        </Map>
-      </div>
-    )
-  }
+  const position = values.centerCoordinates
+
+  return (
+    <div>
+      <p>{layer}</p>
+      <Map
+        center={position}
+        zoom={mapZoom}
+        className={MapStyles.main}
+        style={{ height: "80vh" }} // Workaround for correct map display
+      >
+        <TileLayer
+          attribution={values.attribution}
+          url={values.tileSource}
+          ext={values.ext}
+        />
+        {renderWeatherStations()}
+      </Map>
+    </div>
+  )
 }
+
 
 export default MapContainer
