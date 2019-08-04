d827e35ca5c5702f5b88733766958e46c4210f6a
Michal Muszynski <m.muszy@gmail.com>
2019-08-04 09:37:31 +0200

    Define types for API element

--- src/api/api-types.ts
+++ src/api/api-types.ts
@@ -1,16 +1,25 @@
-interface location {
+interface Location {
   type: string
   crs: any
   coordinates: [number, number]
 }
+
 export interface IStation {
   id: number
   code: string
   street: string
-  location: location
+  location: Location
   lastUpdate: string
   weatherStationId: number
   airTemperature: number
   surfaceTemperature: number
   windDirection: number
+}
+
+export interface IParking {
+  id: number
+  parkingId: number
+  capacity: number
+  freePlaces: number
+  update: string
 }
