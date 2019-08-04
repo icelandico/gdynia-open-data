7a56c21d1ad3307697920fde9bf7c010d20b2146
Michal Muszynski <m.muszy@gmail.com>
2019-08-04 19:49:41 +0200

    Footer as pure elements

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -1,19 +1,16 @@
 import * as React from "react"
-import styles from "./footer-styles"
-import { Footer as FooterCmp, Content } from "rbx"
 import "rbx/index.css"
 import classNames from "classnames"
+import styles from "./footer-styles"
 
-class Footer extends React.Component {
-  render() {
-    return (
-      <FooterCmp>
-        <Content className={styles.center}>
-          <p>created by </p>
-        </Content>
-      </FooterCmp>
-    )
-  }
+const Footer: React.FC = () => {
+  return (
+    <footer>
+      <div className={styles.center}>
+        <p>created by </p>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
