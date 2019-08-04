997e810a5f63ba148699334d211e1d133da03858
Michal Muszynski <m.muszy@gmail.com>
2019-08-04 09:42:18 +0200

    Minor edit fix

--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -1,15 +1,13 @@
 import * as React from "react"
-import styles from "./navbar-styles"
 import { Navbar, Notification } from "rbx"
 import "rbx/index.css"
 import classNames from "classnames"
+import styles from "./navbar-styles"
 
 const NavbarComponent: React.FC = () => {
   return (
     <div>
-      <Notification className={styles.center}>
-        Gdynia Open Data Viewer
-      </Notification>
+      <Notification className={styles.center}>Gdynia Open Data Viewer</Notification>
     </div>
   )
 }
