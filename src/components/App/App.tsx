247e5e09be05b35937291ff1a4792e8321b9db85
Michal Muszynski <m.muszy@gmail.com>
2019-08-04 10:30:37 +0200

    Class to functional component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,22 +1,19 @@
 import * as React from "react"
+import classNames from "classnames"
 import "./App-styles.ts"
 import styles from "./App-styles"
-import "rbx/index.css"
-import classNames from "classnames"
-import MainContainer from "./../MainContainer/main-container-page"
+import MainContainer from "../MainContainer/main-container-page"
 import NavbarComponent from "../Navbar/navbar"
 import Footer from "../Footer/footer"
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className={styles.main}>
-        <NavbarComponent />
-        <MainContainer />
-        {/* <Footer /> */}
-      </div>
-    )
-  }
+const App: React.FC = () => {
+  return (
+    <div className={styles.main}>
+      <NavbarComponent />
+      <MainContainer />
+      <Footer />
+    </div>
+  )
 }
 
 export default App
