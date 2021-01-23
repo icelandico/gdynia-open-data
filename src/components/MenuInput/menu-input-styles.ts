0c93184b077db96528d42dc596dbfd8f29161cae
Michal Muszynski <m.muszy@gmail.com>
2021-01-23 18:27:18 +0100

    Update npm packages

--- src/components/MenuInput/menu-input-styles.ts
+++ src/components/MenuInput/menu-input-styles.ts
@@ -64,46 +64,8 @@ const menuCheckmark = style({
   height: "25px",
   width: "25px",
   backgroundColor: '#eee',
-
-  $nest: {
-      '&'
-  }
 })
 
-/* On mouse-over, add a grey background color */
-.container:hover input ~ .checkmark {
-  background-color: #ccc;
-}
-
-/* When the checkbox is checked, add a blue background */
-.container input:checked ~ .checkmark {
-  background-color: #2196F3;
-}
-
-/* Create the checkmark/indicator (hidden when not checked) */
-.checkmark:after {
-  content: "";
-  position: absolute;
-  display: none;
-}
-
-/* Show the checkmark when checked */
-.container input:checked ~ .checkmark:after {
-  display: block;
-}
-
-/* Style the checkmark/indicator */
-.container .checkmark:after {
-  left: 9px;
-  top: 5px;
-  width: 5px;
-  height: 10px;
-  border: solid white;
-  border-width: 0 3px 3px 0;
-  -webkit-transform: rotate(45deg);
-  -ms-transform: rotate(45deg);
-  transform: rotate(45deg);
-}
 
 const styles = {
   menuLabel,
--- src/components/MenuInput/menu-input.tsx
+++ src/components/MenuInput/menu-input.tsx
@@ -1,6 +1,6 @@
 import React, { FunctionComponent } from "react"
 import { changeLayer } from "../../store/app-store"
-import styles from "./menu-input-styles"
+import "./menu-input.scss";
 
 const MenuInput: FunctionComponent = () => {
   const switchLayer = (layer: any) => changeLayer(layer)
@@ -17,47 +17,47 @@ const MenuInput: FunctionComponent = () => {
         <input
         name="chosen-layer"
         type="radio"
-        className={styles.menuInput}
+        className="checkbox__input"
         value="none"
         onChange={handleChange}
         defaultChecked
         id="none"
       />
-      <span className={styles.menuCheckmark}></span>
+      <span className="checkbox__indicator"></span>
     </label>
-      <input
-        name="chosen-layer"
-        type="radio"
-        className={styles.menuInput}
-        value="weather"
-        onChange={handleChange}
-        id="weather"
-      />
-      <label htmlFor="weather" className={styles.menuLabel}>
-        Pogoda
-      </label>
-      <input
-        name="chosen-layer"
-        type="radio"
-        className={styles.menuInput}
-        value="parkings"
-        onChange={handleChange}
-        id="parkings"
-      />
-      <label htmlFor="parkings" className={styles.menuLabel}>
-        Parkingi
-      </label>
-      <input
-        name="chosen-layer"
-        type="radio"
-        className={styles.menuInput}
-        value="traffic"
-        onChange={handleChange}
-        id="traffic"
-      />
-      <label htmlFor="traffic" className={styles.menuLabel}>
-        Dane o ruchu
-      </label>
+      {/*<input*/}
+      {/*  name="chosen-layer"*/}
+      {/*  type="radio"*/}
+      {/*  className={styles.menuInput}*/}
+      {/*  value="weather"*/}
+      {/*  onChange={handleChange}*/}
+      {/*  id="weather"*/}
+      {/*/>*/}
+      {/*<label htmlFor="weather" className={styles.menuLabel}>*/}
+      {/*  Pogoda*/}
+      {/*</label>*/}
+      {/*<input*/}
+      {/*  name="chosen-layer"*/}
+      {/*  type="radio"*/}
+      {/*  className={styles.menuInput}*/}
+      {/*  value="parkings"*/}
+      {/*  onChange={handleChange}*/}
+      {/*  id="parkings"*/}
+      {/*/>*/}
+      {/*<label htmlFor="parkings" className={styles.menuLabel}>*/}
+      {/*  Parkingi*/}
+      {/*</label>*/}
+      {/*<input*/}
+      {/*  name="chosen-layer"*/}
+      {/*  type="radio"*/}
+      {/*  className={styles.menuInput}*/}
+      {/*  value="traffic"*/}
+      {/*  onChange={handleChange}*/}
+      {/*  id="traffic"*/}
+      {/*/>*/}
+      {/*<label htmlFor="traffic" className={styles.menuLabel}>*/}
+      {/*  Dane o ruchu*/}
+      {/*</label>*/}
     </>
   )
 }
