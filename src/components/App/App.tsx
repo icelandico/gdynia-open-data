19b96850902513b52475fa622ec0060c61af1ca4
Michal Muszynski <m.muszy@gmail.com>
2021-01-23 19:32:06 +0100

    Rename component, change styles

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,13 +2,13 @@ import * as React from "react"
 import "./App-styles.ts"
 import styles from "./App-styles"
 import MainContainer from "../MainContainer/main-container-page"
-import NavbarComponent from "../Navbar/navbar"
+import Header from "../Header/header"
 import Footer from "../Footer/footer"
 
 const App: React.FC = () => {
   return (
     <div className={styles.main}>
-      <NavbarComponent />
+      <Header />
       <MainContainer />
       <Footer />
     </div>
---
+++ src/components/Header/header.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import "./header.scss"
+
+const Header: React.FC = () => {
+  return (
+      <div className="header">Gdynia Open Data Viewer</div>
+  )
+}
+
+export default Header
