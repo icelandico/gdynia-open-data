c1c4565a97c9666ff45dfb6ce1c5ea0b0d1470b5
Michal Muszynski <m.muszy@gmail.com>
2021-01-23 19:48:14 +0100

    Fix styles in layout

--- src/components/MenuPanel/menu-panel.tsx
+++ src/components/MenuPanel/menu-panel.tsx
@@ -5,7 +5,7 @@ import MenuInput from "../MenuInput/menu-input"
 const MenuPanel: React.FC = () => {
   return (
     <div className="menu__container">
-      <p>Wybierz warstwę</p>
+      <h2 className="menu__header">Wybierz warstwę</h2>
       <div className="menu__content">
         <MenuInput />
       </div>
