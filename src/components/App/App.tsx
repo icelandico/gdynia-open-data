9151f19c8b52d6e073ba1803e0355b378f31bf76
Michal Muszynski <m.muszy@gmail.com>
2021-01-23 19:49:13 +0100

    Remove footer

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,11 @@
 import * as React from "react"
 import "./app.scss";
 import MainContainer from "../MainContainer/main-container-page"
-import Footer from "../Footer/footer"
 
 const App: React.FC = () => {
   return (
     <div className="main__container">
       <MainContainer />
-      <Footer />
     </div>
   )
 }
