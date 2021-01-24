2978a797168db32853eb337f0e60672174c80e2d
Michal Muszynski <m.muszy@gmail.com>
2021-01-24 15:55:15 +0100

    Load city boundaries from geojson

--- src/components/Map/map-container.tsx
+++ src/components/Map/map-container.tsx
@@ -1,18 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
-import { Map, TileLayer } from "react-leaflet";
+import { Map, Polygon, TileLayer } from "react-leaflet";
 import values from "../../global/values";
 import WeatherLayer from "../Layers/Weather/weather-layer";
 import ParkingsLayer from "../Layers/Parkings/parkings-layer";
 import "./map-container.scss";
 import RoadSegmentsLayer from "../Layers/RoadSegments/road-segments-layer";
+import boundaries from "../../geojson/boundaries.json";
+import L from "leaflet";
 
 interface IMapContainer {
   activeLayer: string;
 }
 
-const MapContainer: React.FC<IMapContainer> = ( { activeLayer }) => {
+const MapContainer: React.FC<IMapContainer> = ({ activeLayer }) => {
+  const [cityBoundaries, setBoundaries] = useState<number[][]>([])
   const [mapZoom] = useState<number>(12);
+
+  useEffect(() => {
+    const cityPolygon = boundaries.features[0].geometry.coordinates[0];
+    setBoundaries(cityPolygon)
+  });
+
+  const convertCoords = (coords: number[][]) => {
+    return coords.map(
+      (coordSet: number[]) => new L.LatLng(coordSet[1], coordSet[0])
+    );
+  };
 
   const renderLayer = () => {
     switch (activeLayer) {
@@ -41,6 +55,7 @@ const MapContainer: React.FC<IMapContainer> = ( { activeLayer }) => {
           url={values.tileSource}
           ext={values.ext}
         />
+        <Polygon positions={convertCoords(cityBoundaries)} color="#797676" fillColor="transparent" />
         {renderLayer()}
       </Map>
     </div>
