63e4a051ce67e7274bd9c1f369c3b102099657d2
Michal Muszynski <m.muszy@gmail.com>
2021-01-24 14:10:10 +0100

    Remove mock files

--- src/components/Map/map-container.tsx
+++ src/components/Map/map-container.tsx
@@ -8,7 +8,6 @@ import WeatherLayer from "../Layers/Weather/weather-layer";
 import ParkingsLayer from "../Layers/Parkings/parkings-layer";
 import "./map-container.scss";
 import RoadSegmentsLayer from "../Layers/RoadSegments/road-segments-layer";
-import Loader from "../Loader/loader";
 
 const MapContainer: React.FC = () => {
   const layer = useStore(activeLayer);
