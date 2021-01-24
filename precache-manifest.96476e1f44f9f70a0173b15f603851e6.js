93f115da69226f4cf4abe25d9d5a05b7e5189f6d
Michal Muszynski <m.muszy@gmail.com>
2021-01-24 14:58:06 +0100

    Updates

---
+++ precache-manifest.96476e1f44f9f70a0173b15f603851e6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "efeb4ea945329f4ea46e",
+    "url": "/gdynia-open-data/static/css/main.23189884.chunk.css"
+  },
+  {
+    "revision": "efeb4ea945329f4ea46e",
+    "url": "/gdynia-open-data/static/js/main.53900d8a.chunk.js"
+  },
+  {
+    "revision": "d71037d03cbff5d84d29",
+    "url": "/gdynia-open-data/static/js/runtime~main.78b81845.js"
+  },
+  {
+    "revision": "da9e3ad1c339bbbf4119",
+    "url": "/gdynia-open-data/static/js/2.f40530ce.chunk.js"
+  },
+  {
+    "revision": "414164e5100bd02709d4b67345ff8988",
+    "url": "/gdynia-open-data/static/media/navigation.414164e5.svg"
+  },
+  {
+    "revision": "6a7000a0ca8cd28bb41d2bd6de7820dd",
+    "url": "/gdynia-open-data/static/media/marker.6a7000a0.svg"
+  },
+  {
+    "revision": "63ef69d0265a6bcc32a8e7857fd2e226",
+    "url": "/gdynia-open-data/static/media/parking-marker.63ef69d0.svg"
+  },
+  {
+    "revision": "5126b2b5fa38721b281ccfdec0327889",
+    "url": "/gdynia-open-data/index.html"
+  }
+];
