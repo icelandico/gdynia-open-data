1f0c069ee1b638d3e7c5b7f5207f775b919147b1
michal <mail@michalmuszynski.com>
2023-01-06 14:26:43 +0100

    Fix Autor bottom tab

--- src/components/InfoTab/info-tab.tsx
+++ src/components/InfoTab/info-tab.tsx
@@ -4,12 +4,7 @@ import "./info-tab.scss";
 const InfoTab = () => {
   return (
     <div className="info-tab__container">
-      <a
-        className="info-tab__author"
-        href="https://michalmuszynski.com"
-        target="_blank"
-        rel="noreferrer"
-      >
+      <a className="info-tab__author" href="https://geodev.me" target="_blank" rel="noreferrer">
         Autor
       </a>
     </div>
