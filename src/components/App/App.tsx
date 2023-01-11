623fc1545fd4f3efbeedcf376bd6a37b8b70dcd8
michal <mail@michalmuszynski.com>
2023-01-11 21:55:31 +0100

    Install Talkr package for localization

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,15 +1,20 @@
 import * as React from "react";
 import "./app.scss";
+import { Talkr } from "talkr";
 import MainContainer from "../MainContainer/main-container-page";
 import { APIProvider } from "../../context/Provider";
+import en from "../../locales/en/translation.json";
+import pl from "../../locales/pl/translation.json";
 
 const App: React.FC = () => {
   return (
-    <APIProvider>
-      <div className="app__container">
-        <MainContainer />
-      </div>
-    </APIProvider>
+    <Talkr languages={{ en, pl }} defaultLanguage="pl">
+      <APIProvider>
+        <div className="app__container">
+          <MainContainer />
+        </div>
+      </APIProvider>
+    </Talkr>
   );
 };
 
