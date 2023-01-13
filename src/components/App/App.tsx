1a241769da0e39391778309a039fff77a4256975
Michal Muszynski <m.muszy@gmail.com>
2023-01-13 21:14:30 +0100

    Fix translations

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,8 +3,8 @@ import "./app.scss";
 import { Talkr } from "talkr";
 import MainContainer from "../MainContainer/main-container-page";
 import { APIProvider } from "../../context/Provider";
-import en from "../../locales/en/translation.json";
-import pl from "../../locales/pl/translation.json";
+import en from "../../locales/en_translation.json";
+import pl from "../../locales/pl_translation.json";
 
 const App: React.FC = () => {
   return (
